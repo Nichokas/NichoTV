3e4ea0b5924890e7bd5660ef4f460609b17fb617
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-03-21 15:33:02 +0000

    MTV

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirectToF1() { window.location.href = "https://nichokas.github.io/NichoTV/ver/F1/"; }
+
+function redirectToLaLiga() { window.location.href = "https://nichokas.github.io/NichoTV/ver/LaLiga/"; }
+
+function redirectTomtv() { window.location.href = "https://nichokas.github.io/NichoTV/ver/mtv/"; }
