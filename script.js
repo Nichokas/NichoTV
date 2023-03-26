b9d8a4322ce02d83c398a867f8e4fc48e6409d71
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-03-26 09:16:53 +0000

    Migracion a github pages (pages.dev)

--- script.js
+++ script.js
@@ -3,33 +3,33 @@
 
 
 
-function redirectToF1() { window.location.href = "https://nichokas.github.io/NichoTV/ver/F1/"; }
+function redirectToF1() { window.location.href = "https://nichotv.pages.dev/ver/F1/"; }
 
-function redirectToLaLiga() { window.location.href = "https://nichokas.github.io/NichoTV/ver/LaLiga/"; }
+function redirectToLaLiga() { window.location.href = "https://nichotv.pages.dev/ver/LaLiga/"; }
 
-function redirectTomtv() { window.location.href = "https://nichokas.github.io/NichoTV/ver/mtv/"; }
+function redirectTomtv() { window.location.href = "https://nichotv.pages.dev/ver/mtv/"; }
 
-function redirectTomtvhits() { window.location.href = "https://nichokas.github.io/NichoTV/ver/mtvhits/"; }
+function redirectTomtvhits() { window.location.href = "https://nichotv.pages.dev/ver/mtvhits/"; }
 
-function redirectToNG() { window.location.href = "https://nichokas.github.io/NichoTV/ver/natgeo/"; }
+function redirectToNG() { window.location.href = "https://nichotv.pages.dev/ver/natgeo/"; }
 
-function redirectToUCo() { window.location.href = "https://nichokas.github.io/NichoTV/ver/universalcomedy/"; }
+function redirectToUCo() { window.location.href = "https://nichotv.pages.dev/ver/universalcomedy/"; }
 
-function redirectToUCr() { window.location.href = "https://nichokas.github.io/NichoTV/ver/universalcrime/"; }
+function redirectToUCr() { window.location.href = "https://nichotv.pages.dev/ver/universalcrime/"; }
 
-function redirectToUCi() { window.location.href = "https://nichokas.github.io/NichoTV/ver/universalcinema/"; }
+function redirectToUCi() { window.location.href = "https://nichotv.pages.dev/ver/universalcinema/"; }
 
-function redirectToU() { window.location.href = "https://nichokas.github.io/NichoTV/ver/universal/"; }
+function redirectToU() { window.location.href = "https://nichotv.pages.dev/ver/universal/"; }
 
-function redirectToWB() { window.location.href = "https://nichokas.github.io/NichoTV/ver/wb/"; }
+function redirectToWB() { window.location.href = "https://nichotv.pages.dev/ver/wb/"; }
 
-function redirectToHBO() { window.location.href = "https://nichokas.github.io/NichoTV/ver/hbo/"; }
+function redirectToHBO() { window.location.href = "https://nichotv.pages.dev/ver/hbo/"; }
 
-function redirectToHBOxtreme() { window.location.href = "https://nichokas.github.io/NichoTV/ver/hboxtreme/"; }
+function redirectToHBOxtreme() { window.location.href = "https://nichotv.pages.dev/ver/hboxtreme/"; }
 
-function redirectToHBOfam() { window.location.href = "https://nichokas.github.io/NichoTV/ver/hbofam/"; }
+function redirectToHBOfam() { window.location.href = "https://nichotv.pages.dev/ver/hbofam/"; }
 
-function redirectTonick() { window.location.href = "https://nichokas.github.io/NichoTV/ver/nick/"; }
+function redirectTonick() { window.location.href = "https://nichotv.pages.dev/ver/nick/"; }
 
 function openMovistar() {
   window.open("https://tv.movistar.com.ar/tv-guide/epg");
