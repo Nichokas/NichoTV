a41776bd474d5ce4fdb0e232ca80e0192faa3b3b
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-05-27 12:30:04 +0200

    3/3

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-  window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
-    cronitor('config', { clientKey: '6a88f88b1c577341fca211e79dd01e49' });
+window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+cronitor('config', { clientKey: '6a88f88b1c577341fca211e79dd01e49' });
 
-
-
-function redirectToF1() { window.location.href = "https://nichotv.pages.dev/ver/F1/"; }
-
-function redirectToLaLiga() { window.location.href = "https://nichotv.pages.dev/ver/LaLiga/"; }
-
-function redirectTomtv() { window.location.href = "https://nichotv.pages.dev/ver/mtv/"; }
-
-function redirectTomtvhits() { window.location.href = "https://nichotv.pages.dev/ver/mtvhits/"; }
-
-function redirectToNG() { window.location.href = "https://nichotv.pages.dev/ver/natgeo/"; }
-
-function redirectToUCo() { window.location.href = "https://nichotv.pages.dev/ver/universalcomedy/"; }
-
-function redirectToUCr() { window.location.href = "https://nichotv.pages.dev/ver/universalcrime/"; }
-
-function redirectToUCi() { window.location.href = "https://nichotv.pages.dev/ver/universalcinema/"; }
-
-function redirectToU() { window.location.href = "https://nichotv.pages.dev/ver/universal/"; }
-
-function redirectToWB() { window.location.href = "https://nichotv.pages.dev/ver/wb/"; }
-
-function redirectToHBO() { window.location.href = "https://nichotv.pages.dev/ver/hbo/"; }
-
-function redirectToHBOxtreme() { window.location.href = "https://nichotv.pages.dev/ver/hboxtreme/"; }
-
-function redirectToHBOfam() { window.location.href = "https://nichotv.pages.dev/ver/hbofam/"; }
-
-function redirectTonick() { window.location.href = "https://nichotv.pages.dev/ver/nick/"; }
+function redirectTo(channel) {
+  window.location.href = `https://nichotv.pages.dev/ver/${channel}/`;
+}
 
 function openMovistar() {
   window.open("https://www.directv.com.ar/guia/guia.aspx");
